8f9be718d6e856c4b821348d30b8cdc63538b1bc
Gia Edgington <gia.edgington@gmail.com>
2020-04-07 12:40:09 -0400

    Delete products

--- controllers/admin.js
+++ controllers/admin.js
@@ -87,7 +87,7 @@ exports.getProducts = (req, res, next) => {
 exports.postDeleteProduct = (req, res, next) => {
     const prodId = req.body.productId;
     //console.log(prodId);
-    Product.deleteById(prodId)
+    Product.findByIdAndRemove(prodId)
     .then(() => {
         console.log('DESTROYED PRODUCT');
         res.redirect('/admin/products');
